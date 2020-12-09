4e8fd98be7f840eaa8693b1882e80cf628e21ca4
Jings <ljsdd710312601@163.com>
2020-12-09 13:21:28 +0800

    搜索组件

--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
+import store from './store';
 
 const instance = axios.create({
   baseURL: 'https://mallapi.duyiedu.com/',
 });
 
 // 创建一个请求拦截
-instance.interceptors.request.use((config) => config,
-  (error) => Promise.reject(error));
+instance.interceptors.request.use((config) => {
+  if (config.url.includes('/passport')) {
+    // 如果url包含这个字段，说明请求是不需要appkey
+    return config;
+  } // 如果需要aapkey，重新配置config
+  return {
+    ...config,
+    params: {
+      ...config.params,
+      appkey: store.state.user.appkey,
+    },
+  };
+},
+(error) => Promise.reject(error));
 
 // 创建一个响应拦截
 instance.interceptors.response.use((response) => {
